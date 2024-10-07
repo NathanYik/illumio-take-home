d5806e360211c2f4cc669fae3911a29406cd71d9
Pawnnub <nathany094@gmail.com>
2024-10-07 15:03:43 -0700

    Finished README.md and split code into functions

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# illumio-take-home
+# Illumio Take Home
+
+## Assumptions made
+1. Given that there are many protocols, my program only handles tcp, udp, and icmp, as given in the example lookup_table.csv. Further protocol codes can be added later.
+2. The program only handles the default log format and only version 2.
+
+## Steps to run
+1. Install python3
+2. run `python3 main.py`
+3. The program will generate the two output files requested, they will be named `port_and_protocol_combinations.csv` and `tag_counts.csv`
+
+ ## Notes
+ 1. This code only looks at the `dstport` and `protocol` columns in every line of `flow_logs.txt` (columns 7 and 8 respectively), the rest of the row is ignored.
--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import csv
 from collections import defaultdict
 
-def main():
-    protocol_number_mappings = {
-        '6': 'tcp',
-        '17': 'udp'
-    }
+def get_tag_mappings(file_name):
     tag_mappings = {}
-    with open('lookup_table.csv') as csvfile:
-        csv_reader = csv.DictReader(csvfile)
+    with open(file_name) as file:
+        csv_reader = csv.DictReader(file)
         for row in csv_reader:
             tag_mappings[(row["dstport"], row["protocol"])] = row["tag"]
+    return tag_mappings
 
-    with open('flow_logs.txt') as file:
-        port_n_protocol_counts = defaultdict(int)
-        tag_counts = defaultdict(int)
+def get_tag_counts_and_port_n_protocol_counts(file_name, protocol_number_mappings, tag_mappings):
+    port_n_protocol_counts = defaultdict(int)
+    tag_counts = defaultdict(int)
+    with open(file_name) as file:
         for line in file:
             s = line.split()
             key = (s[6], protocol_number_mappings[s[7]])
@@ -23,20 +21,36 @@ def main():
                 tag_counts[tag_mappings[key]] += 1
             else:
                 tag_counts["Untagged"] += 1
+    return [port_n_protocol_counts, tag_counts]
+
+def write_tag_count_file(file_name, tag_counts):
+    with open(file_name, 'w') as tag_count_file:
+        writer = csv.writer(tag_count_file)
+        writer.writerow(['Tag', 'Count'])
+        for tag, count in tag_counts.items():
+            writer.writerow([tag, count])
+
+def write_port_n_protocol_file(file_name, port_n_protocol_counts):
+    with open(file_name, 'w') as port_n_protocol_file:
+        writer = csv.writer(port_n_protocol_file)
+        writer.writerow(['Port', 'Protocol', 'Count'])
+        for (port, protocol), count in port_n_protocol_counts.items():
+            writer.writerow([port, protocol, count])
 
 
-        with open('tag_counts.csv', 'w') as tag_count_file:
-            writer = csv.writer(tag_count_file)
-            writer.writerow(['Tag', 'Count'])
-            for tag, count in tag_counts.items():
-                writer.writerow([tag, count])
+def main():
+    protocol_number_mappings = {
+        '1': 'icmp',
+        '6': 'tcp',
+        '17': 'udp'
+    }
+    tag_mappings = get_tag_mappings("lookup_table.csv")
 
+    port_n_protocol_counts, tag_counts = get_tag_counts_and_port_n_protocol_counts("flow_logs.txt", protocol_number_mappings, tag_mappings)
 
-        with open('port_and_protocol_combinations.csv', 'w') as port_n_protocol_file:
-            writer = csv.writer(port_n_protocol_file)
-            writer.writerow(['Port', 'Protocol', 'Count'])
-            for (port, protocol), count in port_n_protocol_counts.items():
-                writer.writerow([port, protocol, count])
+    write_tag_count_file("tag_counts.csv", tag_counts)
+
+    write_port_n_protocol_file("port_and_protocol_combinations.csv", port_n_protocol_counts)
 
 
 if __name__ == "__main__":
